fd2056ca72af3569789431ce8c91620ff1f6db11
민승 김 <rlaalstmd0606@gmail.com>
2024-01-30 12:58:38 +0900

    위치 설정

--- Api_Project/Kirby_Game/Stage1_Level.cpp
+++ Api_Project/Kirby_Game/Stage1_Level.cpp
@@ -23,8 +23,8 @@ void UStage1_Level::BeginPlay()
 	// NewPath.Move("ContentsResources");
 	NewPath.MoveParent();
 
-	NewPath.Move("ContentsResources");
-	NewPath.Move("Texture");
+	NewPath.Move("GameResources");
+	NewPath.Move("kirby_resources");
 
 
 	// 확장자도 마찬가지 대소문자 구분을 무조건 대문자로 바꿔서 찾을것이다..
