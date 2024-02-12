466792f4f0e6c5fef9e77c1bd9f45fb9aad2e7d6
민승 김 <rlaalstmd0606@gmail.com>
2024-02-12 16:26:16 +0900

    Game_Core 주석 추가

--- Api_Project/Kirby_Game/Game_Core.cpp
+++ Api_Project/Kirby_Game/Game_Core.cpp
@@ -19,22 +19,19 @@ UGame_Core::~UGame_Core()
 // 게임시작
 void UGame_Core::BeginPlay() // 
 {
-	MainWindow.SetWindowPosition({ 200,100 }); // 
-	MainWindow.SetWindowScale({ 1120/* * 1.5f*/, 810/* * 1.5f*/ }); // 1024,960
 	UEngineCore::BeginPlay();
-	// "Title Level" + "을 만들다가 에러가 났습니다";
 
-	// 기본 커비 리소스만 여기다가 로드
-	// 실행경로가 나와야 한다고 생각합니다.
+	MainWindow.SetWindowPosition({ 200,100 }); // 윈도우창이 뜨는 위치 Set
+	MainWindow.SetWindowScale({ 1120, 810}); // 윈도우창 크기 Set
+
 	UEngineDirectory NewPath; // 현재 파일 경로
 
-	// NewPath.Move("ContentsResources");
-	NewPath.MoveParent(); // 현재 파일 경로의 상위 폴더로 이동
+	NewPath.MoveParent(); // 현재 파일 경로의 상위(부모) 폴더로 이동
 
 	// 커비의 기본 움직임 이미지는 모든 스테이지에서 사용하므로 코어에서 로드
 	NewPath.Move("GameResources"); // 현재 폴더에서 다른 폴더로 이동
-	NewPath.Move("kirby_resources"); // 
-	NewPath.Move("kirby_Base"); // 
+	NewPath.Move("kirby_resources"); // 경로 이동
+	NewPath.Move("kirby_Base"); // 경로 이동
 
 
 	// 확장자도 마찬가지 대소문자 구분을 무조건 대문자로 바꿔서 찾을것이다..
@@ -46,16 +43,20 @@ void UGame_Core::BeginPlay() //
 		// 싱글톤 잊지 말라고 일부러 GetInst를 사용하겠습니다.
 		UEngineResourcesManager::GetInst().LoadImg(File.GetFullPath()); // 로딩 -> Map(Iamges)
 	}
+	
+	// WindowImage std::vector<UImageInfo> Infos; 에서 컷한 이미지 스케일, 위치 관리
 	UEngineResourcesManager::GetInst().CuttingImage("kirby_Right.png", 10, 7);
 	UEngineResourcesManager::GetInst().CuttingImage("kirby_Left.png", 10, 7);
 	UEngineResourcesManager::GetInst().CuttingImage("kirby2_Right.png", 10, 6);
 	UEngineResourcesManager::GetInst().CuttingImage("kirby2_Left.png", 10, 6);
 
 	SetFrame(60); //-> 프로그래머가 컴퓨터 프레임을 고정시킬수 있다.
-	CreateLevel<UTitleLevel>("TitleLevel");
-	CreateLevel<UStage1_Level>("Stage1_Level");
-	ChangeLevel("TitleLevel");
+
+	// EngineCore에서 std::map<std::string, ULevel*> AllLevel; map으로 관리
+	CreateLevel<UTitleLevel>("TitleLevel"); // 타이틀 레벨
+	CreateLevel<UStage1_Level>("Stage1_Level"); // stage1_Level
 	
+	ChangeLevel("TitleLevel"); // 레벨 변경
 }
 
 void UGame_Core::Tick(float _DeltaTime)
