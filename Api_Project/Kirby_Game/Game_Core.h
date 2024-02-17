c8db6ce158083a39fb47397ed0e45e14101e8ad5
민승 김 <rlaalstmd0606@gmail.com>
2024-02-17 16:17:50 +0900

    주석 정리중

--- Api_Project/Kirby_Game/Game_Core.h
+++ Api_Project/Kirby_Game/Game_Core.h
@@ -1,13 +1,13 @@
 #pragma once
 #include <EngineCore\EngineCore.h>
 
-// 설명 :
-class UGame_Core : public UEngineCore
+class UGame_Core : public UEngineCore // 상속
 {
 public:
-	// constrcuter destructer
-	UGame_Core();
-	~UGame_Core();
+	
+	// 클래스 디폴트
+	UGame_Core(); // 디폴트 생성자
+	~UGame_Core(); // 디폴트 소멸자
 
 	// delete Function
 	UGame_Core(const UGame_Core& _Other) = delete;
@@ -16,10 +16,10 @@ public:
 	UGame_Core& operator=(UGame_Core&& _Other) noexcept = delete;
 
 protected:
-	// 부모인 
-	void BeginPlay() override;
-	void Tick(float _DeltaTime) override;
-	void End() override;
+	// 오버라이드 
+	void BeginPlay() override; // 시작
+	void Tick(float _DeltaTime) override; // 업데이트
+	void End() override; // 종료
 
 private:
 
