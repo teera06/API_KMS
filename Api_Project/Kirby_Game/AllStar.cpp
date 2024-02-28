f82d919fdd5182689040e56506793755ca878e4a
민승 김 <rlaalstmd0606@gmail.com>
2024-02-28 15:29:28 +0900

    중간 커밋

--- Api_Project/Kirby_Game/AllStar.cpp
+++ Api_Project/Kirby_Game/AllStar.cpp
@@ -2,6 +2,7 @@
 #include "ModeEnum.h"
 #include "Monster_Base.h"
 #include "pengi_Ice.h"
+#include "Monster_Fire.h"
 
 AAllStar::AAllStar()
 {
@@ -102,6 +103,35 @@ void AAllStar::Collisiongather(float _DeltaTime)
 		Monster->Destroy(0.3f);
 		Destroy();
 	}
+	else if (true == AllStarCollision->CollisionCheck(ECollisionOrder::FireMonster, Result))
+	{
+		// 이런식으로 상대를 사용할수 있다.
+		UCollision* Collision = Result[0];
+		AActor* Ptr = Collision->GetOwner();
+		AMonster_Fire* Monster = dynamic_cast<AMonster_Fire*>(Ptr);
+
+
+		if (nullptr == Monster)
+		{
+			MsgBoxAssert("터져야겠지....");
+		}
+
+		if (MonsterDirNormal.iX() == -1) // 몬스터가 플레이어를 향하는 방향의 반대 방향으로 힘이 작용
+		{
+			Monster->GetMonsterRenderer()->ChangeAnimation("die_Left"); // 죽는 애니메이션
+
+		}
+		else {
+			Monster->GetMonsterRenderer()->ChangeAnimation("die_Right"); // 죽는 애니메이션
+		}
+		FVector DiePos = MonsterDirNormal * -200.0f * _DeltaTime * FVector::Right; // 죽으면서 이동하는 위치 계산
+		Monster->SetIsDie(true);
+		Monster->SetDiePos(DiePos);
+		Monster->Destroy(0.3f);
+		Destroy();
+	}
+
+
 
 	// 픽셀 충돌
 	Color8Bit ColorR = UActorCommon::ColMapImage->GetColor(GetActorLocation().iX(), GetActorLocation().iY() - 20, Color8Bit::RedA);
