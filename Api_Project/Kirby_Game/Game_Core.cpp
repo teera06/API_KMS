a111b128c8f398629408528461f0a6091b8a8c63
민승 김 <rlaalstmd0606@gmail.com>
2024-02-29 11:39:51 +0900

    중간 커밋

--- Api_Project/Kirby_Game/Game_Core.cpp
+++ Api_Project/Kirby_Game/Game_Core.cpp
@@ -3,7 +3,7 @@
 // Level 생성에 필요한 헤더
 #include "TitleLevel.h"
 #include "Stage1_Level.h"
-
+#include "Stage2_Level.h"
 
 
 
@@ -29,7 +29,7 @@ void UGame_Core::BeginPlay()
 	// EngineCore에서 std::map<std::string, ULevel*> AllLevel; map으로 관리
 	CreateLevel<UTitleLevel>("TitleLevel"); // 타이틀 Level 생성
 	CreateLevel<UStage1_Level>("Stage1_Level"); // stage1_Level 생성
-	//CreateLevel<UStage2_Level>("Stage1_Level"); // stage1_Level 생성
+	CreateLevel<UStage2_Level>("Stage2_Level"); // stage1_Level 생성
 	
 	ChangeLevel("TitleLevel"); // 레벨 변경
 }
