fde4674cad118fd35856490874e4b4afc8dc0180
민승 김 <rlaalstmd0606@gmail.com>
2024-03-04 15:56:03 +0900

    중간 커밋

---
+++ Api_Project/Kirby_Game/Stage3_Level.cpp
@@ -0,0 +1,25 @@
+#include "Stage3_Level.h"
+
+Stage3_Level::Stage3_Level()
+{
+}
+
+Stage3_Level::~Stage3_Level()
+{
+}
+
+void Stage3_Level::BeginPlay()
+{
+}
+
+void Stage3_Level::Tick(float _DeltaTime)
+{
+}
+
+void Stage3_Level::LevelStart(ULevel* _PrevLevel)
+{
+}
+
+void Stage3_Level::LevelEnd(ULevel* _NextLevel)
+{
+}
---
+++ Api_Project/Kirby_Game/Stage3_Level.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <EngineCore\Level.h> // 상속
+#include <EnginePlatform\EngineSound.h>
+
+//class AStage2_Map;
+class AKirby_Player;
+class AKirby_HpBar;
+class AMonster_Sir;
+class Apengi_Ice;
+
+class Stage3_Level :public ULevel
+{
+public:
+	// constrcuter destructer
+	Stage3_Level(); // 디폴트 생성자
+	~Stage3_Level(); // 디폴트 소멸자
+
+	// delete Function
+	Stage3_Level(const Stage3_Level& _Other) = delete; // 디폴트 복사 생성자
+	Stage3_Level(Stage3_Level&& _Other) noexcept = delete; 
+	Stage3_Level& operator=(const Stage3_Level& _Other) = delete; // 디폴트 대입 연산자
+	Stage3_Level& operator=(Stage3_Level&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+	void LevelStart(ULevel* _PrevLevel)override;
+	void LevelEnd(ULevel* _NextLevel) override;
+private:
+	UEngineSoundPlayer BGMPlayer;
+
+	/AStage1_MAP* NewMap = nullptr;
+	AKirby_Player* NewPlayer = nullptr;
+	AKirby_HpBar* NewHpBar = nullptr;
+};
+
