efd60042ca841eb05586486be8fbe211dadb8eeb
민승 김 <rlaalstmd0606@gmail.com>
2024-03-05 12:46:41 +0900

    중간 커밋

--- Api_Project/Kirby_Game/SubBoss.cpp
+++ Api_Project/Kirby_Game/SubBoss.cpp
@@ -18,13 +18,49 @@ void SubBoss::Tick(float _DeltaTime)
 
 }
 
-void SubBoss::CalDir()
+void SubBoss::CalDir(float _DeltaTime)
 {
 	FVector PlayerPos = MainPlayer->GetActorLocation();  // 플레이어 위치
 	FVector MonsterPos = GetActorLocation(); // 몬스터 위치
 
+	FVector AttXL = MonsterPos + FVector::Left * AttRange; // 몬스터 왼쪽 플레이어 인식 시야 X축
+	FVector AttXR = MonsterPos + FVector::Right * AttRange; // 몬스터 오른쪽 플레이어 인식 시야 X축
+
+	FVector PlayerX = PlayerPos * FVector::Right; // 플레이어 위치 X축
+
 	FVector MonsterDir = PlayerPos - MonsterPos; // 플레이어 위치 - 몬스터 위치 = 방향 ex) 몬스터가 플레이어에게 향하는 방향
 	MonsterDirNormal = MonsterDir.Normalize2DReturn();  // 해당값을 정규화 
+
+	MovePos = FVector::Zero; // 플레이어 추격 시 이동
+
+	if (MonsterDirNormal.iX() == -1) // 왼쪽 방향
+	{
+		MonsterRenderer->ChangeAnimation("Move_Left");
+		WallX = -20;
+	}
+	else if (MonsterDirNormal.iX() == 1 && IsIce == false) { // 오른쪽 방향
+		MonsterRenderer->ChangeAnimation("Move_Right");
+		WallX = 20;
+	}
+	MovePos += MonsterDirNormal * _DeltaTime * MoveSpeed * FVector::Right; // 몬스터가 플레이어의 Y축도 인식할 수 있으니 FVector::Right 를 곱해 X축만 추격
+
+	// 플레이어를 향해 공격
+	if (AttXL.iX() < PlayerX.iX() && AttXR.iX() > PlayerX.iX() && MainPlayer->GetActorLocation().iY() >= GetActorLocation().iY() - 30) // 몬스터 시야에 포착된 경우 X축 기준 왼쪽, 오른쪽
+	{
+		IsAtt = true;
+	}
+}
+
+void SubBoss::IceAtt()
+{
+}
+
+void SubBoss::Collisiongather(float _DeltaTime)
+{
+}
+
+void SubBoss::CalResult(float _DeltaTime)
+{
 }
 
 void SubBoss::GroundUp()
@@ -41,4 +77,21 @@ void SubBoss::GroundUp()
 			break;
 		}
 	}
+}
+
+void SubBoss::MoveUpdate(float _DeltaTime)
+{
+}
+
+void SubBoss::AniCreate()
+{
+	MonsterRenderer->CreateAnimation("Move_Right", "Tock_Right.png", 7, 9, 0.3f, true);
+	MonsterRenderer->CreateAnimation("Move_Left", "Tock_Left.png", 7, 9, 0.3f, true);
+	MonsterRenderer->CreateAnimation("Att_Right", "Tock_Right.png", 4, 6, 0.15f, false);
+	MonsterRenderer->CreateAnimation("Att_Left", "Tock_Left.png", 4, 6, 0.15f, false);
+
+	MonsterRenderer->CreateAnimation("die_Right", "Tock_Left.png", 11, 13, 0.3f, true); // 죽음 
+	MonsterRenderer->CreateAnimation("die_Left", "Tock_Right.png",11, 13, 0.3f, true); // 죽음 
+	
+	//MonsterRenderer->CreateAnimation("Effect", "Effects.png", 29, 30, 0.1f, true); // 죽는 애니메이션
 }
--- Api_Project/Kirby_Game/SubBoss.h
+++ Api_Project/Kirby_Game/SubBoss.h
@@ -18,6 +18,11 @@ public:
 	SubBoss& operator=(const SubBoss& _Other) = delete; // 디폴트 대입 연산자
 	SubBoss& operator=(SubBoss&& _Other) noexcept = delete;
 
+	UImageRenderer* GetMonsterRenderer()
+	{
+		return MonsterRenderer;
+	}
+
 protected:
 	// TickObject (Level2) 오버라이드
 	void BeginPlay() override;
@@ -29,8 +34,35 @@ private:
 	UImageRenderer* MonsterRenderer = nullptr; // 커비 랜더링 (이미지)
 	UCollision* MonsterCollision = nullptr;
 
-	void CalDir();
+	FVector StartDir = FVector::Left; // 몬스터 초반 이동 방향
+	FVector MovePos = FVector::Zero;
+
+	void MoveUpdate(float _DeltaTime); // 몬스터 최종 움직임 제어
+
+	void AniCreate(); // 애니메이션 관리
+
+	void IceToMonster(float _DeltaTime);
+
+	void CalDir(float _DeltaTime);
+	void IceAtt();
+
+	void Collisiongather(float _DeltaTime);
+	void CalResult(float _DeltaTime);
+
 
 	void GroundUp();
+
+	bool BaseOn = false; // 커비에게 흡수 당할 때 확인 
+	bool IsDie = false; // 죽은 상태 인지 확인
+	bool IsIce = false; // 얼음 상태인지 확인
+	bool IsAtt = false;
+	float skillcooldowntime = 0.0f;
+
+	int WallX = 0; // 벽에 충돌할때 X축 범위
+
+	float MoveSpeed = 50.0f; // 몬스터 스피드
+	const float IceSpeed = 350.0f; // 얼음(얼려진 후) 이동 스피드
+	const float sight = 250.0f; // 몬스터 시야
+	const float AttRange = 150.0f; // 몬스터 공격 범위
 };
 
